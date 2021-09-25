4a0fd13c116d017a3fee6473a573df625c8508dd
nobody <nobody@nowhere.mars>
2021-09-25 18:34:24 +0800

    Update writer.lua in Luna plug-in.

--- V2RayGCon/Resources/Files/lua/modules/writer.lua
+++ V2RayGCon/Resources/Files/lua/modules/writer.lua
@@ -3,16 +3,29 @@ local Writer = {}
 -- settings
 local DefaultFilename = "LuaData.txt"
 
+local function ToString(content)
+    if type(content) == "string" then
+        return content
+    end
+    return tostring(content)
+end
+
 -- helper functions
 local function WriteToFile(filename, content)
 	local file = io.open(filename, "a")
-    file:write(tostring(content) .. "\n")
+    file:write(ToString(content) .. "\n")
     file:close()
 end
 
 local function ClearFile(filename)
 	local file = io.open(filename, "w+")
 	file:close()
+end
+
+function Writer:WriteAllText(content)
+    local file = io.open(self.filename, "w+")
+    file:write(ToString(content))
+    file:close()
 end
 
 function Writer:WriteLine(content)

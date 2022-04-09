5e6824c6ff29f1e960bd037454c0e6ac38c6d0eb
nobody <nobody@nowhere.mars>
2022-04-09 16:25:50 +0800

    Remove json related functions in Luna plug-in.

--- V2RayGCon/Libs/Lua/ApiComponents/UtilsApi.cs
+++ V2RayGCon/Libs/Lua/ApiComponents/UtilsApi.cs
@@ -9,61 +9,6 @@ namespace V2RayGCon.Libs.Lua.ApiComponents
         VgcApis.BaseClasses.Disposable,
         VgcApis.Interfaces.Services.IUtilsService
     {
-        #region json
-        public bool TrySetValue<T>(JToken json, string path, T value) =>
-            Misc.Utils.TrySetValue<T>(json, path, value);
-
-        public T GetValue<T>(JToken json, string path) =>
-            Misc.Utils.GetValue<T>(json, path);
-
-        public JToken GetKey(JToken json, string path) =>
-            Misc.Utils.GetKey(json, path);
-
-        public string GetProtocol(JObject config) =>
-            Misc.Utils.GetProtocolFromConfig(config);
-
-        public string JTokenToString(JToken jtoken) =>
-            jtoken.ToString();
-
-        public void Replace(JToken node, JToken value) =>
-            node.Replace(value);
-
-        public void Union(JObject body, JObject mixin) =>
-            Misc.Utils.UnionJson(body, mixin);
-
-        public void Merge(JObject body, JObject mixin) =>
-            Misc.Utils.MergeJson(body, mixin);
-
-        public void CombineWithRoutingInTheEnd(JObject body, JObject mixin) =>
-            Misc.Utils.CombineConfigWithRoutingInTheEnd(ref body, mixin);
-
-        public void CombineWithRoutingInFront(JObject body, JObject mixin) =>
-           Misc.Utils.CombineConfigWithRoutingInFront(ref body, mixin);
-
-        public JObject ToJObject(JToken jtoken) =>
-            jtoken as JObject;
-
-        public JArray ToJArray(JToken jtoken) =>
-            jtoken as JArray;
-
-        public JToken ParseJToken(string json)
-        {
-            try
-            {
-                return JToken.Parse(json);
-            }
-            catch { }
-            return null;
-        }
-
-        public JArray ParseJArray(string json) =>
-            ParseJToken(json) as JArray;
-
-        public JObject ParseJObject(string json) =>
-            ParseJToken(json) as JObject;
-
-        #endregion
-
         #region misc
         public string AddLinkPrefix(string linkBody, VgcApis.Models.Datas.Enums.LinkTypes type) =>
             Misc.Utils.AddLinkPrefix(linkBody, type);
--- VgcApis/Interfaces/Services/IUtilsService.cs
+++ VgcApis/Interfaces/Services/IUtilsService.cs
@@ -6,26 +6,6 @@ namespace VgcApis.Interfaces.Services
 {
     public interface IUtilsService
     {
-        #region json
-        bool TrySetValue<T>(JToken json, string path, T value);
-        T GetValue<T>(JToken json, string path);
-
-        void CombineWithRoutingInFront(JObject body, JObject mixin);
-        void CombineWithRoutingInTheEnd(JObject body, JObject mixin);
-        JToken GetKey(JToken json, string path);
-        string GetProtocol(JObject config);
-        string JTokenToString(JToken jtoken);
-        void Merge(JObject body, JObject mixin);
-        JToken ParseJToken(string json);
-        JArray ParseJArray(string json);
-        JObject ParseJObject(string json);
-        void Replace(JToken node, JToken value);
-        JArray ToJArray(JToken jtoken);
-        JObject ToJObject(JToken jtoken);
-        void Union(JObject body, JObject mixin);
-
-        #endregion
-
         #region misc        
         string AddLinkPrefix(string linkBody, VgcApis.Models.Datas.Enums.LinkTypes type);
         string Base64Encode(string plainText);

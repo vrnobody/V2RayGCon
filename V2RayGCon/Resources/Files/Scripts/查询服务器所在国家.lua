733289cd0edce63e207a69e579c3ae3f5cea699e
nobody <nobody@nowhere.mars>
2023-03-19 19:21:46 +0800

    Bump version to v1.6.8.0

--- V2RayGCon/Resources/Files/Scripts/查询服务器所在国家.lua
+++ V2RayGCon/Resources/Files/Scripts/查询服务器所在国家.lua
@@ -1,26 +1,15 @@
 --[[
 查询服务器所在国家然后保存到tag1中
-2021-09-22
+2023-03-19
 --]]
 
 local geoAPI = "http://ip-api.com/json/"
 
+local utils = require('lua.libs.utils')
 local json = require('lua.libs.json')
 
 local proxyPort = Web:GetProxyPort()
-local servs = Server:GetAllServers()
-
-function IsDupSummary(summary, index)
-    for i = 0, index - 1 do
-        local coreServ = servs[i]
-        local coreState = coreServ:GetCoreStates()
-        local s = coreState:GetSummary()
-        if s == summary then
-            return true
-        end
-    end
-    return false
-end
+local cache = {}
 
 function GetHost(summary)
     local t = {}
@@ -38,48 +27,54 @@ function GetGeoInfo(host)
     local resp = Web:Fetch(url, proxyPort, 5000)
     Misc:Sleep(300)
     local o = json.decode(resp)
-    return o["country"]
+    return o["countryCode"]
 end
 
 function TryGetGeoInfo(host)
-    for i = 0, 10 do
+    local c = cache[host]
+    if c ~= nil then
+        print("using cache for ", host)
+        return c
+    end
+    for i = 0, 5 do
+        if Signal:Stop() then
+            return nil
+        end
         local ok, country = pcall(GetGeoInfo, host)
         if ok then
+            cache[host] = country
             return country
         end
         Misc:Sleep(5000)
-        print("Error: ", country)
+        print("error: ", country)
     end
     return nil
 end
 
-function Proc(coreServ, index)
+function Proc(coreServ)
     local coreState = coreServ:GetCoreStates()
+    local tag1 = coreState:GetTag1()
+    if not string.isempty(tag1) then
+        return
+    end
     local summary = coreState:GetSummary()
-    if not IsDupSummary(summary, index) then
-        local host = GetHost(summary)
-        if host ~= nil then
-            local title = coreState:GetTitle()
-            local country = TryGetGeoInfo(host)
-            if country ~= nil then
-                coreState:SetTag1(country)
-                print(title, " -> ", country)
-            else
-                print("Get GEO info failed: ", title)
-            end
+    local host = GetHost(summary)
+    if host ~= nil then
+        local title = coreState:GetTitle()
+        local country = TryGetGeoInfo(host)
+        if country ~= nil then
+            coreState:SetTag1(country)
+            print(title, " -> ", country)
+        else
+            print("fail to get GEO info: ", title)
         end
     end
 end
 
 function Main()
-    local max = servs.Count
-    for i = 0, max - 1 do
-        local coreServ = servs[i]
-        local coreState = coreServ:GetCoreStates()
-        local tag1 = coreState:GetTag1()
-        if string.isempty(tag1) then
-            Proc(coreServ, i)
-        end
+    local servs = Server:GetAllServers()
+    for coreServ in Each(servs) do
+        Proc(coreServ, i)
         if Signal:Stop() then
             return
         end

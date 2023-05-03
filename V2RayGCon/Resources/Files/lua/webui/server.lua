5a3c3f4d540aef183dc5058d7a15b388e93eb9df
nobody <nobody@nowhere.mars>
2023-05-03 20:07:17 +0800

    Update web UI.

--- V2RayGCon/Resources/Files/lua/webui/server.lua
+++ V2RayGCon/Resources/Files/lua/webui/server.lua
@@ -2,19 +2,16 @@
 local url = "http://localhost:4000/"
 local public = "./lua/webui"
 
+-- confings
+local version = "0.0.1.0"
 local pageSize = 50
 
+-- code
 local haServ = require('lua.modules.httpServ').new()
 local json = require('lua.libs.json')
 local utils = require('lua.libs.utils')
 
-function Main()
-    haServ:Create(url, public, handler, false)
-    print("请打开网址: ", url)
-    haServ:Run()
-end
-
-function Clamp(v, min, max)
+local function Clamp(v, min, max)
     if v < min then
         return min
     end
@@ -24,12 +21,22 @@ function Clamp(v, min, max)
     return v
 end
 
-function GetPageNumByIndex(index)
-    local pageNum =math.ceil(index / pageSize)
-    return tostring(pageNum)
+local function Response(ok, result)
+    local r = {
+        ["ok"] = ok,
+        ["r"] = result,
+    }
+    return json.encode(r)
 end
 
-function GetSerializedServers(pageNum)
+function GetPageNumByIndex(param)
+    local index = #param > 0 and tonumber(param[1]) or 1
+    local pageNum = math.ceil(index / pageSize)
+    return pageNum
+end
+
+function GetSerializedServers(ps)
+    local pageNum = #ps > 0 and tonumber(ps[1]) or 1
     local servs = Server:GetAllServers()
     if servs.Count < 1 then
         return json.encode({
@@ -52,7 +59,7 @@ function GetSerializedServers(pageNum)
         local coreCtrl = coreServ:GetCoreCtrl()
         local t = {}
         t["index"] = coreState:GetIndex()
-        t["name"] = coreState:GetShortName()
+        t["name"] = coreState:GetName()
         t["summary"] = coreState:GetSummary()
         t["uid"] = coreState:GetUid()
         t["on"] = coreCtrl:IsCoreRunning()
@@ -64,65 +71,71 @@ function GetSerializedServers(pageNum)
         pages = 1
     end
     
-    local r = {
+    return {
         ["pages"] = pages,
         ["data"] = d,
     }
-    
-    -- print("d: ", #d)
-    return json.encode(r)
 end 
 
-function RestartServ(uid)
+function RestartServ(ps)
+    local uid = #ps > 0 and ps[1] or ""
     local coreServ = utils.GetFirstServerWithUid(uid)
     if coreServ ~= nil then
         local coreCtrl = coreServ:GetCoreCtrl()
         Server:StopAllServers()
         coreCtrl:RestartCore()
-        return "ok"
+        return true
     end
-    return "unknow uid: " .. uid
+    return false
 end
 
-function StopServ(uid)
+function StopServ(ps)
+    local uid = #ps > 0 and ps[1] or ""
     local coreServ = utils.GetFirstServerWithUid(uid)
     if coreServ ~= nil then
         local coreCtrl = coreServ:GetCoreCtrl()
         coreCtrl:StopCore()
-        return "ok"
+        return true
     end
-    return "unknow uid: " .. uid
+    return false
+end
+
+function GetServerVersion()
+    return version
 end
 
 function GetSettings()
-    local s = Misc:GetUserSettings()
-    return s
+    return Misc:GetUserSettings()
 end
 
-function handler(req)
-    
-    -- print("req: ", req)
-    
-    local j = json.decode(req)
-    local op = j["fn"]
-    
-    -- print(table.dump(j))
-    -- print("fn: ", op, "param: ", table.dump(j["ps"]))
-    
-    if op == "GetSettings" then
-        return GetSettings()
-    elseif op == "GetPageNumByIndex" then
-        local index = #j["ps"] > 0 and tonumber(j["ps"][1]) or 1
-        return GetPageNumByIndex(index)
-    elseif op == "GetSerializedServers" then
-        local pn = #j["ps"] > 0 and tonumber(j["ps"][1]) or 1
-        return GetSerializedServers(pn)
-    elseif op == "RestartServ" then
-        return RestartServ(j["ps"][1])
-    elseif op == "StopServ" then
-        return StopServ(j["ps"][1])
+local function Handler(req)
+    local ok, j = pcall(json.decode, req)
+    if not ok then
+        print("parse request error: ", req)
+        return Response(false, "parse request error")
     end
-    return "unknow req: " .. req
+    
+    local fn = j["fn"]
+    local f = _G[fn]
+    local p = j["ps"]
+    if type(f) ~= "function" then
+        return Response(false, "function not found")
+    end
+    if type(p) ~= "table" then
+        return Response(false, "params is not a table")
+    end
+    
+    local ok, r = pcall(f, p)
+    if ok then
+        return Response(true, r)
+    end
+    return Response(false, "call " .. fn .. " error: " .. r)
+end
+
+local function Main()
+    haServ:Create(url, public, Handler, false)
+    print("请打开网址: ", url)
+    haServ:Run()
 end
 
 Main()

1a34e38918a1eb881b0b4c3cc0a2b4ab307690eb
nobody <nobody@nowhere.mars>
2023-05-05 16:28:56 +0800

    Modify logger.lua

--- V2RayGCon/Resources/Files/lua/modules/logger.lua
+++ V2RayGCon/Resources/Files/lua/modules/logger.lua
@@ -10,21 +10,35 @@ local function WriteLine(filename, content)
     file:close()
 end
 
+Logger.logLevels = {
+    ["Debug"] = 8,
+    ["Info"] = 4,
+    ["Warn"] = 2,
+    ["Error"] = 1,
+}
 
 function Logger:Warn(text)
-	self:Log("Warn", text)
+    if self.logLevel >= Logger.logLevels["Warn"] then
+        self:Log("Warn", text)
+    end
 end
 
 function Logger:Debug(text)
-	self:Log("Debug", text)
+    if self.logLevel >= Logger.logLevels["Debug"] then
+        self:Log("Debug", text)
+    end
 end
 
 function Logger:Error(text)
-	self:Log("Error", text)
+    if self.logLevel >= Logger.logLevels["Error"] then
+        self:Log("Error", text)
+    end
 end
 
 function Logger:Info(text)
-	self:Log("Info", text)
+    if self.logLevel >= Logger.logLevels["Info"] then
+        self:Log("Info", text)
+    end
 end
 
 function Logger:Log(prefix, content)
@@ -35,16 +49,29 @@ function Logger:Log(prefix, content)
 	end
 	local timestamp = os.date("[%Y-%m-%d %X]")
 	local line = timestamp .. prefix .. tostring(content)
-	WriteLine(self.filename, line)
+    if string.isempty(self.filename) then
+        print(line)
+    else
+        WriteLine(self.filename, line)
+    end
 end
 
-function Logger.new(filename)
-    
+function Logger:SetLogLevel(logLevel)
+    if logLevel == nil or not table.contains(Logger.logLevels, logLevel) then
+        print("unsupported log level: ", logLevel)
+        logLevel = Logger.logLevels["Debug"]
+    end
+    self.logLevel = logLevel
+end
+
+function Logger.new(filename, logLevel)
 	local o = {
-        filename = filename or DefaultLogFilename
+        filename = filename,
+        logLevel = Logger.logLevels["Debug"]
     }
     
     setmetatable(o, {__index = Logger})
+    o:SetLogLevel(logLevel)
 	return o
 end
 

709d4490f3ddb6c607d9c4b13551ea9027fbd1a1
nobody <nobody@nowhere.mars>
2023-09-15 09:36:52 +0800

    Add Luna plug-in into GitHub release assets.

--- V2RayGCon/Properties/AssemblyInfo.cs
+++ V2RayGCon/Properties/AssemblyInfo.cs
@@ -33,11 +33,12 @@ using System.Runtime.InteropServices;
 // 可以指定所有值，也可以使用以下所示的 "*" 预置版本号和修订号
 // 方法是按如下所示使用“*”: :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.8.3.14")]
+[assembly: AssemblyVersion("1.8.3.15")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 
 /*
 
+v1.8.3.15 Add Luna plug-in into GitHub release assets.
 v1.8.3.14 (WIP) Remove v://... decoder.
                 Set custom core name while importing sharelink.
 v1.8.3.13 (WIP) Add custom core settings.

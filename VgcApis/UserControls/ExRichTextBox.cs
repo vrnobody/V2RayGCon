f0b3575974e745d6fa73d6bf21ea9f82413b364e
nobody <nobody@nowhere.mars>
2023-09-17 23:47:13 +0800

    Fix bug.

--- VgcApis/UserControls/ExRichTextBox.cs
+++ VgcApis/UserControls/ExRichTextBox.cs
@@ -29,18 +29,16 @@ namespace VgcApis.UserControls
 
         const int WM_MOUSEWHEEL = 0x020A;
 
-        //thanks to a-clymer's solution
         protected override void WndProc(ref Message m)
         {
             if (m.Msg == WM_MOUSEWHEEL)
             {
-                //directly send the message to parent without processing it
+                //send the message to parent
+                //thanks to a-clymer's solution
                 //according to https://stackoverflow.com/a/19618100
                 SendMessage(this.Parent.Handle, m.Msg, m.WParam, m.LParam);
-                m.Result = IntPtr.Zero;
             }
-            else
-                base.WndProc(ref m);
+            base.WndProc(ref m);
         }
 
         protected override CreateParams CreateParams

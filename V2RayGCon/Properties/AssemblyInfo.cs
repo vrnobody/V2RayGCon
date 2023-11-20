28aeae2f1d70d1e5e751ba754ed220226f64006a
nobody <nobody@nowhere.mars>
2023-11-21 07:15:10 +0800

    Remove wintun.dll from V2RayGCon.zip

--- V2RayGCon/Properties/AssemblyInfo.cs
+++ V2RayGCon/Properties/AssemblyInfo.cs
@@ -33,11 +33,14 @@ using System.Runtime.InteropServices;
 // 可以指定所有值，也可以使用以下所示的 "*" 预置版本号和修订号
 // 方法是按如下所示使用“*”: :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.8.8.7")]
+[assembly: AssemblyVersion("1.8.8.8")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 
 /*
 
+v1.8.8.8 Remove wintun.dll from V2RayGCon.zip
+         Add more options in batch modifier
+         Supports sock outbound in simple editor
 v1.8.8.7 Supports disable vmess/vless sharelink decoders.
          Add std.Web:FetchSocks5().
          Add std.Server:AddNew().
--- V2RayGCon/Resources/Files/3rd/tun2socks/readme.txt
+++ V2RayGCon/Resources/Files/3rd/tun2socks/readme.txt
@@ -1,5 +1,8 @@
 ﻿我也不想在源码里带dll，但是GitHub action下载wintun会报500错误。
-这个目录里的wintun.dll是v0.14.1 amd64
+
+V2RayGCon-box.zip里面的wintun.dll是v0.14.1 amd64
 sha256sum e5da8447dc2c320edc0fc52fa01885c103de8c118481f683643cacc3220dafce
 
-2023-11-15
+tun2socks是GitHub action自己下的。
+
+2023-11-21

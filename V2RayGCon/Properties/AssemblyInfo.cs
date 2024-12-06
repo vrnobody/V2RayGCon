1b953bab0704aea1fb1383c03e2befdebea9d0f4
nobody <nobody@nowhere.mars>
2024-12-06 11:19:59 +0800

    v2.0.8.0

--- V2RayGCon/Properties/AssemblyInfo.cs
+++ V2RayGCon/Properties/AssemblyInfo.cs
@@ -33,11 +33,12 @@ using System.Runtime.InteropServices;
 // 可以指定所有值，也可以使用以下所示的 "*" 预置版本号和修订号
 // 方法是按如下所示使用“*”: :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.7.2")]
+[assembly: AssemblyVersion("2.0.8.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 
 /*
 
+--------------------------------------------------------------------
 v2.0.7.2 Add std.Misc:GetNeoLunaLogAsString() in NeoLuna plug-in.
          Fix typo std.Sys:Lu[s -> a]ServSetIndex().
 v2.0.7.1 Support xhttp and raw stream settings in vless://...

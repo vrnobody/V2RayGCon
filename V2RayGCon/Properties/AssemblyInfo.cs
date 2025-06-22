59a644c2bbab2b1a722ab0d4854a019ac1152688
nobody <nobody@nowhere.mars>
2025-06-22 11:47:04 +0800

    Auto-scale progress bar in FormImportLinksResult.

--- V2RayGCon/Properties/AssemblyInfo.cs
+++ V2RayGCon/Properties/AssemblyInfo.cs
@@ -33,11 +33,12 @@ using System.Runtime.InteropServices;
 // 可以指定所有值，也可以使用以下所示的 "*" 预置版本号和修订号
 // 方法是按如下所示使用“*”: :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.1.1.4")]
+[assembly: AssemblyVersion("2.1.1.5")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 
 /*
 
+v2.1.1.5 Auto-scale progress bar in FormImportLinksResult.
 v2.1.1.4 Add subscription shortcut in WelcomeUI.
 v2.1.1.3 Switch default PAC domain source to Loyalsoldier/v2ray-rules-dat.
 v2.1.1.2 Refactor TicketPool.

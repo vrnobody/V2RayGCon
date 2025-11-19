a712fc86694b71ec73108e8ec73bccf249c15702
nobody <nobody@nowhere.mars>
2025-11-19 17:04:03 +0800

    v2.2.1.0

--- V2RayGCon/Properties/AssemblyInfo.cs
+++ V2RayGCon/Properties/AssemblyInfo.cs
@@ -33,14 +33,15 @@ using System.Runtime.InteropServices;
 // 可以指定所有值，也可以使用以下所示的 "*" 预置版本号和修订号
 // 方法是按如下所示使用“*”: :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.2.0.4")]
+[assembly: AssemblyVersion("2.2.1.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 
 // xray-core v25.9.11 simplify vless outbound config
-// plan to modify vless://... decoder after 2026-01-01
+// postpone vless://... decoder support
 
 /*
 
+--------------------------------------------------------------------
 v2.2.0.4 Disable proxy port setting under Auto mode in ProxySetter plug-in.
 v2.2.0.3 Fix GetAssemblyVersion() return null bug.
 v2.2.0.2 Fix #31 auto-track feature not working bug.

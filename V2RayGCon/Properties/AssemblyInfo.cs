f80f075c9b98481280815996ce22e6d8037186cb
nobody <nobody@nowhere.mars>
2026-05-09 11:41:36 +0800

    Add icons to I/O dialog in NeoLuna plug-in.

--- V2RayGCon/Properties/AssemblyInfo.cs
+++ V2RayGCon/Properties/AssemblyInfo.cs
@@ -33,7 +33,7 @@ using System.Runtime.InteropServices;
 // 可以指定所有值，也可以使用以下所示的 "*" 预置版本号和修订号
 // 方法是按如下所示使用“*”: :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.2.8.2")]
+[assembly: AssemblyVersion("2.2.8.3")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 
 // xray-core v25.9.11 simplify vless outbound config
@@ -43,6 +43,7 @@ using System.Runtime.InteropServices;
 
 /*
 
+v2.2.8.3 Add icons to I/O dialog in NeoLuna plug-in.
 v2.2.8.2 Add copy-output feature in NeoLuna editor.
 v2.2.8.1 Using round buttons.
 --------------------------------------------------------------------
